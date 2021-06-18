ad4bd41495d063b708e79b9133831986f6248494
Everton <tonrs00@gmail.com>
2021-06-17 21:32:58 -0300

    Dominio

---
+++ Banco/Dominio/Entidades/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPFCNPJ { get; set; }
+        public virtual List<Contato> Contatos { get; set; }
+
+    }
+}
---
+++ Banco/Dominio/Entidades/Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entidades
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public string ClienteId { get; set; }
+        public virtual Cliente Cliente { get; set; }
+        public string Tipo { get; set; }
+        public int NRContra { get; set; }
+        public Decimal Saldo { get; set; }
+
+    }
+}
